c7389339a44de04ac8eec0350c6c96c37219be59
Tetyana Yakobchuk <tetyana.yakobchuk@gmail.com>
2020-12-05 12:20:54 +0200

    Update project-modals.js

--- js/project-modals.js
+++ js/project-modals.js
@@ -1,22 +1,23 @@
-const projectModalFullStack = document.querySelector('#personal-project-full-stack'); //помогает их html файла, когда сайт уже запущен, найти такой элемент, и когда находит ему на кнопку добавляется событие на клик, и при клике добавляется класс и на боди вешается ограничение скрола
+//const projectModalFullStack = document.querySelector('#personal-project-full-stack');
 const projectModalWeddingProject = document.querySelector('#personal-project-wedding-project');
 const projectModaProTest = document.querySelector('#personal-project-pro-test');
-const projectModalProjectGoit = document.querySelector('#personal-project-goit');
+//const projectModalProjectGoit = document.querySelector('#personal-project-goit');
 
 const projectModalQuestify = document.querySelector('#team-project-questify');
 const projectModalItTest = document.querySelector('#team-project-it-test');
 
-const projectOpenBtnFullStack = document.querySelector('#personal-project-full-stack-btn');
+
+//const projectOpenBtnFullStack = document.querySelector('#personal-project-full-stack-btn');
 const projectOpenBtnWeddingProject = document.querySelector('#personal-project-wedding-project-btn');
 const projectOpenBtnProTest = document.querySelector('#personal-project-pro-test-btn');
-const projectOpenBtnProjectGoit = document.querySelector('#personal-project-goit-btn');
+//const projectOpenBtnProjectGoit = document.querySelector('#personal-project-goit-btn');
 
 const projectOpenBtnQuestify = document.querySelector('#team-project-questify-btn');
 const projectOpenBtnItTest = document.querySelector('#team-project-it-test-btn');
 //массив с модалками, позиция проекта соответствует позиции кнопки
-const projectModals = [ projectModalFullStack, projectModalWeddingProject, projectModaProTest, projectModalProjectGoit, projectModalQuestify, projectModalItTest];
+const projectModals = [ /*projectModalFullStack*/, projectModalWeddingProject, projectModaProTest, /*projectModalProjectGoit*/, projectModalQuestify, projectModalItTest];
 //массив с кнопками
-const projectBtns = [projectOpenBtnFullStack, projectOpenBtnWeddingProject, projectOpenBtnProTest, projectOpenBtnProjectGoit, projectOpenBtnQuestify, projectOpenBtnItTest];
+const projectBtns = [/*projectOpenBtnFullStack*/, projectOpenBtnWeddingProject, projectOpenBtnProTest, /*projectOpenBtnProjectGoit*/, projectOpenBtnQuestify, projectOpenBtnItTest];
 
 projectBtns.forEach((btn, index) => {
     const projectModal = projectModals[index];
